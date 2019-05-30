83b8191fe366c51c66fb6927e3f351ed0c10ebe2
Kucherenko Mykyta <Kucherenko Mykyta@users.noreply.github.com>
2019-05-30 17:58:06 +0300

    Rewrote to use messageSource.

--- src/main/java/edu/practice/resourceServer/model/service/impl/UserDetailsServiceImpl.java
+++ src/main/java/edu/practice/resourceServer/model/service/impl/UserDetailsServiceImpl.java
@@ -5,6 +5,8 @@ import edu.practice.resourceServer.model.entity.UserDetailsImpl;
 import edu.practice.resourceServer.model.repository.ApplicationUserInternalRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.data.domain.Example;
 import org.springframework.data.domain.ExampleMatcher;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -19,9 +21,12 @@ public class UserDetailsServiceImpl implements UserDetailsService {
 
     private ApplicationUserInternalRepository applicationUserInternalRepository;
 
+    private MessageSource messageSource;
+
     @Autowired
-    public UserDetailsServiceImpl(ApplicationUserInternalRepository applicationUserInternalRepository) {
+    public UserDetailsServiceImpl(ApplicationUserInternalRepository applicationUserInternalRepository, MessageSource messageSource) {
         this.applicationUserInternalRepository = applicationUserInternalRepository;
+        this.messageSource = messageSource;
     }
 
     @Override
@@ -32,8 +37,13 @@ public class UserDetailsServiceImpl implements UserDetailsService {
                 ExampleMatcher.matching().withIgnoreCase("username"));
         Optional<ApplicationUser> persistedUserOptional = applicationUserInternalRepository.findOne(persistedApplicationUserExample);
         if (!persistedUserOptional.isPresent())
-            // TODO: rewrite in case of introducing messageSource
-            throw new UsernameNotFoundException("User " + username + " not found.");
+            throw new UsernameNotFoundException(
+                messageSource.getMessage(
+                    "userDetailsServiceImpl.usernameNotFoundException.message",
+                    new Object[] {
+                        username
+                    },
+                    LocaleContextHolder.getLocale()));
 
         ApplicationUser persistedUser = persistedUserOptional.get();
         return UserDetailsImpl.builder()
---
+++ src/test/java/edu/practice/resourceServer/ResourceServerUserLifecycleTests.java
@@ -0,0 +1,35 @@
+package edu.practice.resourceServer;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+
+@ExtendWith(SpringExtension.class)
+@AutoConfigureMockMvc
+@SpringBootTest(classes = ResourceServerApplication.class)
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+class ResourceServerUserLifecycleTests {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @BeforeAll
+    static void beforeAll() {
+
+    }
+
+    @Order(1)
+    @Test
+    void userCreationTest() {
+        
+    }
+
+}
